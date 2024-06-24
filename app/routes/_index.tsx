33e1069bfaf3d0487c52780715f2de5bc34109d2
BlisS <fixtergeek@gmail.com>
2024-06-24 17:46:11 -0600

    ci deploy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <article className="h-[100vh] grid place-items-center">
-      <h1 className="text-6xl">Bienvenido a Denik Agenda</h1>
+      <h1 className="text-6xl">Bienvenid@ a Denik Agenda</h1>
+      <p>Tu agenda, tus clientes, tu negocio.</p>
     </article>
   );
 }
