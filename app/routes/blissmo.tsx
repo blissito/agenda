887fd7417c53501dbcb52821c0208e72a93dca42
BlisS <fixtergeek@gmail.com>
2024-06-25 00:25:31 -0600

    volume working

--- app/routes/blissmo.tsx
+++ app/routes/blissmo.tsx
@@ -2,11 +2,18 @@ import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
-export const loader = (props: LoaderFunctionArgs) => {
-  return db.user.findFirst() || {};
+export const loader = async (props: LoaderFunctionArgs) => {
+  // const u = await db.user.findFirst();
+  // if (!u) {
+  //   await db.user.create({
+  //     data: { email: "fixtergeek@gmail.com", name: "blissito", role: "ADMIN" },
+  //   });
+  // }
+  const user = await db.user.findFirst();
+  return { user };
 };
 
 export default function Page() {
-  const user = useLoaderData<typeof loader>();
+  const { user } = useLoaderData<typeof loader>();
   return <h2>Blissmos {user.email}</h2>;
 }
