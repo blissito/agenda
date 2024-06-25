e69cf706a1484d63cf0cd25ce801e5f0c5bbd21b
BlisS <fixtergeek@gmail.com>
2024-06-24 22:22:26 -0600

    test

--- app/routes/blissmo.tsx
+++ app/routes/blissmo.tsx
@@ -1,3 +1,12 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = (props: LoaderFunctionArgs) => {
+  return db.user.findFirst() || {};
+};
+
 export default function Page() {
-  return <h2>Blissmos</h2>;
+  const user = useLoaderData<typeof loader>();
+  return <h2>Blissmos {user.email}</h2>;
 }
