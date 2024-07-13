886cb9bfa8dfb5ccb6d8749f9852787125e1400c
BlisS <fixtergeek@gmail.com>
2024-07-13 12:03:06 -0600

    almost there with the grid gallery, image is morphing

---
+++ app/routes/blissmo.grid-gallery.tsx
@@ -0,0 +1,151 @@
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
+import { createPortal } from "react-dom";
+
+// @TODO: preserving z-20 until animation done
+
+export default function Page() {
+  return (
+    <>
+      <GridGallery
+        images={[
+          "https://images.pexels.com/photos/889545/pexels-photo-889545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "https://images.pexels.com/photos/903171/pexels-photo-903171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "https://images.pexels.com/photos/34514/spot-runs-start-la.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "https://images.pexels.com/photos/591646/cow-moooh-meadow-black-591646.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        ]}
+      />
+    </>
+  );
+}
+
+const GridGallery = ({ images }: { images: string[] }) => {
+  const [selected, set] = useState<null | number>(null);
+  const [cache, setCache] = useState<null | number>(null);
+  const handleClose = async () => set(null);
+  const handleSelect = (index: number) => {
+    setCache(index);
+    set(index);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = ({ key }: KeyboardEvent) => {
+      if (key === "Escape") {
+        handleClose();
+      }
+    };
+    addEventListener("keydown", handleKeyDown);
+    return () => removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  console.log("CACHE: ", cache);
+  console.log("Selected: ", selected);
+
+  return (
+    <article className="flex justify-center items-center h-screen bg-slate-950">
+      <section className="grid grid-cols-3 grid-rows-2 gap-2 w-full h-full p-44">
+        <Image
+          isSelected={selected === 0}
+          onClose={handleClose}
+          onClick={() => handleSelect(0)}
+          className={twMerge("col-span-2")}
+          imageClassName={cache === 0 ? "z-20 relative" : undefined}
+          src={images[0]}
+        />
+        <Image
+          isSelected={selected === 1}
+          src={images[1]}
+          onClose={handleClose}
+          onClick={() => handleSelect(1)}
+          imageClassName={cache === 1 ? "z-20 relative" : undefined}
+        />
+        <Image
+          src={images[2]}
+          isSelected={selected === 2}
+          onClose={handleClose}
+          onClick={() => handleSelect(2)}
+          imageClassName={cache === 2 ? "z-20 relative" : undefined}
+        />
+        <Image
+          isSelected={selected === 3}
+          onClose={handleClose}
+          onClick={() => handleSelect(3)}
+          className="col-span-2"
+          src={images[3]}
+          imageClassName={cache === 3 ? "z-20 relative" : undefined}
+        />
+      </section>
+    </article>
+  );
+};
+
+const Image = ({
+  isSelected,
+  src,
+  className,
+  onClose,
+  onClick,
+  imageClassName,
+}: {
+  onClick?: () => void;
+  onClose?: () => void;
+  isSelected?: boolean;
+  className?: string;
+  imageClassName?: string;
+  src: string;
+}) => {
+  return (
+    <>
+      <section className={twMerge("", className)}>
+        <div
+          className={twMerge("relative w-full h-full", isSelected ? "" : "")}
+        >
+          {isSelected ? (
+            createPortal(
+              <>
+                <div className="py-[10%] px-[10%] absolute inset-0 flex">
+                  <motion.img
+                    role="button"
+                    onClick={onClick}
+                    layoutId={src}
+                    className={twMerge(
+                      "rounded-xl object-cover",
+                      "z-20",
+                      "w-full",
+                      "h-full"
+                    )}
+                    alt="gallery pic"
+                    src={src}
+                  />
+                </div>
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  onClick={onClose}
+                  className={twMerge(
+                    "absolute inset-0 z-10 p-44 bg-slate-100 cursor-pointer"
+                  )}
+                />
+              </>,
+              document.body
+            )
+          ) : (
+            <motion.img
+              role="button"
+              onClick={onClick}
+              layoutId={src}
+              className={twMerge(
+                "rounded-xl w-full h-full object-cover",
+                "absolute inset-0",
+                imageClassName
+              )}
+              alt="gallery pic"
+              src={src}
+            />
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
