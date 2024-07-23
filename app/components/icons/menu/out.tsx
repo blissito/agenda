f3ba38bbcd45de2b4f042823c9abd72762c6d8e1
BrendaOrtega <bremin11.20.93@gmail.com>
2024-07-23 12:57:21 -0600

    sidebar icons update

--- app/components/icons/menu/out.tsx
+++ app/components/icons/menu/out.tsx
@@ -15,20 +15,20 @@ export const Out = ({
     <path
       d="M12.6382 19.7163H6.48115C5.90158 19.7163 5.34576 19.486 4.93595 19.0762C4.52614 18.6664 4.29591 18.1106 4.2959 17.531V6.49196C4.2959 5.91238 4.52614 5.35653 4.93596 4.94671C5.34579 4.53688 5.90163 4.30664 6.48122 4.30664H12.6383"
       stroke={fill}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M9.03711 12H18.9076"
       stroke={fill}
-      stroke-miterlimit="10"
-      stroke-linecap="round"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
     />
     <path
       d="M16.0381 8.33203L19.7042 12.0192L16.0303 15.693"
       stroke={fill}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
