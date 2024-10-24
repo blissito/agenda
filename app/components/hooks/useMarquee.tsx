ac5ba6a5631e72c69bb3e1fa30050af318b2b19d
Blissito <fixtergeek@gmail.com>
2024-10-23 19:32:31 -0600

    simple flipper (new approach)

--- app/components/hooks/useMarquee.tsx
+++ app/components/hooks/useMarquee.tsx
@@ -32,16 +32,21 @@ export const useMarquee = (reversed: boolean = false) => {
     const moveBy = factor * -velocityFactor.get();
     const rect1 = ref.current?.getBoundingClientRect();
     const v = x.get();
-    x.set(v + moveBy); // add
+    x.set(v + moveBy); // <= adding
+    // corrections
+    // when moving to => if x>0 set at left (-w/2)
     if (rect1 && v > 0) {
       x.set(-rect1.width / 2);
       return;
     }
+    // when moving to <= if x<-w/2 set at right (0)
     if (rect1 && v < -(rect1.width / 2)) {
       x.set(0);
       return;
     }
   };
+  // getting frames
   useAnimationFrame(move);
+  // returning useful states
   return { ref, x };
 };
