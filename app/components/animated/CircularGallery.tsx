913b2283a3e87544252249af5f9555042d542b9f
Blissito <fixtergeek@gmail.com>
2024-11-08 15:41:28 -0600

    reusable circular gallery

---
+++ app/components/animated/CircularGallery.tsx
@@ -0,0 +1,203 @@
+import {
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+  useTransform,
+  motion,
+  MotionValue,
+} from "framer-motion";
+import { Children, ReactNode, useRef } from "react";
+import { cn } from "~/utils/cd";
+
+export default function CircularGallery({
+  children,
+  emojis,
+  videos,
+}: {
+  videos: string[];
+  emojis: string[];
+  children: ReactNode;
+}) {
+  const texts = Children.toArray(children);
+  const target = useRef(null);
+  const { scrollYProgress } = useScroll({ target });
+  const scrollSpring = useSpring(scrollYProgress, { bounce: 0.0 });
+  const rotateZ = useTransform(scrollSpring, [0, 1], [-40, 14]);
+
+  // emojies
+  const y = useTransform(scrollSpring, [0, 0.15], ["30%", "0%"]);
+  const opacity = useTransform(scrollSpring, [0, 0.15], [0, 1]);
+
+  const limits = [
+    [0, 0.189, 0.31],
+    [0.31, 0.47, 0.63],
+    [0.63, 0.737, 0.859],
+    [0.859, 0.9],
+  ];
+
+  // swap text
+  const textOneY = useTransform(scrollSpring, limits[0], ["50%", "0%", "-20%"]);
+
+  const textTwoY = useTransform(scrollSpring, limits[1], ["50%", "0%", "-20%"]);
+
+  const textThreeY = useTransform(scrollSpring, limits[2], [
+    "50%",
+    "0%",
+    "-20%",
+  ]);
+
+  const textFourY = useTransform(scrollSpring, limits[3], ["50%", "0%"]);
+
+  // opacities
+  const opacityOne = useTransform(scrollSpring, limits[0], [0, 1, 0]);
+  const opacityTwo = useTransform(scrollSpring, limits[1], [0, 1, 0]);
+  const opacityThree = useTransform(scrollSpring, limits[2], [0, 1, 0]);
+  const opacityFour = useTransform(scrollSpring, limits[3], [0, 1]);
+
+  useMotionValueEvent(scrollSpring, "change", (latest) => {
+    // console.log("scroll: ", latest);
+  });
+
+  return (
+    <>
+      <article>
+        <div className="h-[40vh] bg-pink-500" />
+        {/* Scroll area */}
+        <main className="h-[600vh] lg:h-[400vh] relative" ref={target}>
+          {/* Sticky container */}
+          <section className="sticky h-[100vh] top-0 overflow-hidden pl-[10%]">
+            {/* Emojies section */}
+            <motion.div
+              style={{
+                y,
+                opacity,
+              }}
+              className="w-[36vw] absolute top-0 bottom-0 flex flex-col justify-center gap-4 "
+            >
+              {/* Este margen modifica todo */}
+              <div className="text-4xl flex gap-1 -mt-28">
+                <Emoji style={{ opacity: opacityOne }}>{emojis[0]}</Emoji>
+                <Emoji style={{ opacity: opacityTwo }}>{emojis[1]}</Emoji>
+                <Emoji style={{ opacity: opacityThree }}>{emojis[2]}</Emoji>
+                <Emoji style={{ opacity: opacityFour }}>{emojis[3]}</Emoji>
+              </div>
+
+              <div className="bg-red-500 relative">
+                <Text style={{ y: textOneY, opacity: opacityOne }}>
+                  {texts[0]}
+                </Text>
+                <Text style={{ y: textTwoY, opacity: opacityTwo }}>
+                  {texts[0]}
+                </Text>
+                <Text style={{ y: textThreeY, opacity: opacityThree }}>
+                  {texts[0]}
+                </Text>
+                <Text style={{ y: textFourY, opacity: opacityFour }}>
+                  {texts[0]}
+                </Text>
+              </div>
+            </motion.div>
+            {/* Roatary anim container */}
+            <div className="absolute flex items-center top-0 bottom-0 left-[90%]">
+              <motion.section
+                style={{
+                  rotateZ,
+                  //   y,
+                  aspectRatio: 1,
+                  //   transformStyle: "preserve-3d",
+                }}
+                className={cn(
+                  "justify-center rounded-full",
+                  "flex flex-col gap-y-[1.8%]",
+                  //   "aspect-square"
+                  "w-[250vw]"
+                  //   "w-[70vw]"
+                )}
+              >
+                <Video
+                  className="rotate-[30deg] translate-x-[45%] translate-y-[-7%]"
+                  src={videos[0]}
+                />
+                <Video
+                  className="rotate-[14deg] translate-x-[11%] translate-y-[-7%]"
+                  src={videos[1]}
+                />
+                <Video className="" src={videos[2]} />
+                <Video
+                  className="rotate-[-14deg] translate-x-[11%] translate-y-[7%]"
+                  src={videos[3]}
+                />
+                <Video as="placeholder" />
+              </motion.section>
+            </div>
+          </section>
+        </main>
+        <div className="h-[60vh] bg-yellow-500" />
+      </article>
+    </>
+  );
+}
+
+export const Text = ({
+  children,
+  style,
+}: {
+  style?: Record<string, MotionValue<any>>;
+  children: ReactNode;
+}) => {
+  return (
+    <motion.div style={style} className="text-xs lg:text-lg absolute top-0">
+      {children}
+    </motion.div>
+  );
+};
+
+export const Emoji = ({
+  children,
+  style,
+}: {
+  style?: Record<string, MotionValue<any>>;
+  children?: ReactNode;
+}) => {
+  return (
+    <section className="relative p-8">
+      <motion.span
+        style={style}
+        className="rounded-full absolute inset-0 lg:text-6xl bg-orange-500"
+      ></motion.span>
+      <div className="absolute inset-0 flex justify-center items-center">
+        {children}
+      </div>
+    </section>
+  );
+};
+Emoji.type = "Emoji";
+
+const Video = ({
+  src,
+  className,
+  as,
+  ...props
+}: {
+  as?: "placeholder";
+  className?: string;
+  src?: string;
+}) => {
+  return (
+    <video
+      style={{
+        aspectRatio: 4.76 / 3.54,
+      }}
+      className={cn(
+        "rounded-[50px] object-cover w-[16%] left-[-16%] relative",
+        className
+      )}
+      muted
+      autoPlay
+      loop
+      playsInline
+      src={src}
+      {...props}
+    />
+  );
+};
--- app/routes/blissmo.rotary.tsx
+++ app/routes/blissmo.rotary.tsx
@@ -1,250 +1,56 @@
-import {
-  useMotionValueEvent,
-  useScroll,
-  useSpring,
-  useTransform,
-  motion,
-  MotionValue,
-} from "framer-motion";
-import { ReactNode, useRef } from "react";
-import { cn } from "~/utils/cd";
+import CircularGallery from "~/components/animated/CircularGallery";
 
 export default function Route() {
-  const target = useRef(null);
-  const { scrollYProgress } = useScroll({ target });
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log("scroll, ", latest);
-  });
-
-  const scrollSpring = useSpring(scrollYProgress, { bounce: 0.0 });
-  const rotateZ = useTransform(scrollSpring, [0, 1], [-40, 20]);
-
-  // emojies
-  const y = useTransform(scrollSpring, [0, 0.15], ["30%", "0%"]);
-  const opacity = useTransform(scrollSpring, [0, 0.15], [0, 1]);
-
-  // swap text
-  const textOneY = useTransform(
-    scrollSpring,
-    [0, 0.16, 0.31],
-    ["50%", "0%", "-20%"]
-  );
-
-  const textTwoY = useTransform(
-    scrollSpring,
-    [0.31, 0.42, 0.57],
-    ["50%", "0%", "-20%"]
-  );
-
-  const textThreeY = useTransform(
-    scrollSpring,
-    [0.57, 0.65, 1],
-    ["50%", "0%", "-20%"]
-  );
-
-  const textFourY = useTransform(scrollSpring, [0.77, 0.89], ["50%", "0%"]);
-
-  // op
-  const opacityOne = useTransform(scrollSpring, [0, 0.16, 0.31], [0, 1, 0]);
-  const opacityTwo = useTransform(scrollSpring, [0.31, 0.42, 0.57], [0, 1, 0]);
-  const opacityThree = useTransform(
-    scrollSpring,
-    [0.57, 0.65, 0.77],
-    [0, 1, 0]
-  );
-  const opacityFour = useTransform(scrollSpring, [0.77, 0.89], [0, 1]);
-
-  useMotionValueEvent(scrollSpring, "change", (latest) => {
-    console.log("scroll: ", latest);
-  });
-
   return (
-    <>
-      <article>
-        <div className="h-[40vh] bg-pink-500" />
-        {/* Scroll area */}
-        <main className="h-[600vh] lg:h-[400vh] relative" ref={target}>
-          {/* Sticky container */}
-          <section className="sticky h-[100vh] top-0 overflow-hidden pl-[10%]">
-            {/* Emojies section */}
-            <motion.div
-              style={{
-                y,
-                opacity,
-              }}
-              className="w-[36vw] absolute top-0 bottom-0 flex flex-col justify-center gap-4 "
-            >
-              <div className="text-4xl flex gap-1">
-                <Emoji style={{ opacity: opacityOne }}>🪄</Emoji>
-                <Emoji style={{ opacity: opacityTwo }}>👽</Emoji>
-                <Emoji style={{ opacity: opacityThree }}>🧉</Emoji>
-                <Emoji style={{ opacity: opacityFour }}>🥳</Emoji>
-              </div>
-
-              <div className="relative">
-                <Text
-                  style={{
-                    y: textOneY,
-                    opacity: opacityOne,
-                  }}
-                >
-                  <h2 className="text-3xl lg:text-6xl mb-4">
-                    Ponte chido con el ejercicio
-                  </h2>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Saepe impedit nihil aspernatur repellendus error. Atque
-                  commodi placeat iure ducimus nemo architecto et odio, pariatur
-                  unde adipisci praesentium iusto nobis! Nemo.
-                </Text>
-
-                <Text
-                  style={{
-                    y: textTwoY,
-                    opacity: opacityTwo,
-                  }}
-                >
-                  <h2 className="text-3xl lg:text-6xl mb-4">
-                    O te pondrás marrano
-                  </h2>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Saepe impedit nihil aspernatur repellendus error. Atque
-                  commodi placeat iure ducimus nemo architecto et odio, pariatur
-                  unde adipisci praesentium iusto nobis! Nemo.
-                </Text>
-
-                <Text
-                  style={{
-                    y: textThreeY,
-                    opacity: opacityThree,
-                  }}
-                >
-                  <h2 className="text-3xl lg:text-6xl mb-4">
-                    y ya nadie te va a querer
-                  </h2>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Saepe impedit nihil aspernatur repellendus error. Atque
-                  commodi placeat iure ducimus nemo architecto et odio, pariatur
-                  unde adipisci praesentium iusto nobis! Nemo.
-                </Text>
-                <Text
-                  style={{
-                    y: textFourY,
-                    opacity: opacityFour,
-                  }}
-                >
-                  <h2 className="text-3xl lg:text-6xl mb-4">
-                    Porque así es la pinche gente
-                  </h2>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Saepe impedit nihil aspernatur repellendus error. Atque
-                  commodi placeat iure ducimus nemo architecto et odio, pariatur
-                  unde adipisci praesentium iusto nobis! Nemo.
-                </Text>
-              </div>
-            </motion.div>
-            {/* Roatary anim container */}
-            <div className="absolute flex items-center top-0 bottom-0 left-[90%]">
-              <motion.section
-                style={{
-                  rotateZ,
-                  //   y,
-                  aspectRatio: 1,
-                  //   transformStyle: "preserve-3d",
-                }}
-                className={cn(
-                  "justify-center rounded-full",
-                  "flex flex-col gap-y-[1.8%]",
-                  //   "aspect-square"
-                  "w-[250vw]"
-                  //   "w-[70vw]"
-                )}
-              >
-                <Video
-                  className="rotate-[30deg] translate-x-[45%] translate-y-[-7%]"
-                  src="https://dl.dropboxusercontent.com/s/e7fsnz9e8g5ngwqvk0y8h/24632438_z7AqYnq4.mp4?rlkey=czo2p7d3v35fndfjongci6yqy&st=zoe1fcb1&dl=0"
-                />
-                <Video
-                  className="rotate-[14deg] translate-x-[11%] translate-y-[-7%]"
-                  src="https://dl.dropboxusercontent.com/s/4kle801qkjkz0lw9qpjkt/24632437_cFlqNVLO.mp4?rlkey=ub4hte7ktze5658v66ci6axj8&st=5exnlo0e&dl=0"
-                />
-                <Video
-                  className=""
-                  src="https://dl.dropboxusercontent.com/s/4r1qys3nvzx3y6q3qwewb/24632436_69jCgczl.mp4?rlkey=4va5qw8xmxil8tak52de7fu7e&st=nw71j49v&dl=0"
-                />
-                <Video
-                  className="rotate-[-14deg] translate-x-[11%] translate-y-[7%]"
-                  src="https://dl.dropboxusercontent.com/s/qx0rp5f5h5bcoopp2b0d0/24632435_IdhNXXql.mp4?rlkey=bmthdyn0huc71pro38wa1twcw&st=wqm79hpd&dl=0"
-                />
-                <Video as="placeholder" />
-              </motion.section>
-            </div>
-          </section>
-        </main>
-        <div className="h-[60vh] bg-yellow-500" />
-      </article>
-    </>
+    <CircularGallery
+      emojis={["👽", "🤓", "🎩", "🪄"]}
+      videos={[
+        "https://dl.dropboxusercontent.com/s/e7fsnz9e8g5ngwqvk0y8h/24632438_z7AqYnq4.mp4?rlkey=czo2p7d3v35fndfjongci6yqy&st=zoe1fcb1&dl=0",
+        "https://dl.dropboxusercontent.com/s/4kle801qkjkz0lw9qpjkt/24632437_cFlqNVLO.mp4?rlkey=ub4hte7ktze5658v66ci6axj8&st=5exnlo0e&dl=0",
+        "https://dl.dropboxusercontent.com/s/4r1qys3nvzx3y6q3qwewb/24632436_69jCgczl.mp4?rlkey=4va5qw8xmxil8tak52de7fu7e&st=nw71j49v&dl=0",
+        "https://dl.dropboxusercontent.com/s/qx0rp5f5h5bcoopp2b0d0/24632435_IdhNXXql.mp4?rlkey=bmthdyn0huc71pro38wa1twcw&st=wqm79hpd&dl=0",
+      ]}
+    >
+      <div>
+        <h2 className="text-3xl lg:text-6xl mb-4">
+          Ponte chido con el ejercicio
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          error accusamus iste eligendi quia esse vitae adipisci, pariatur in
+          cumque voluptas possimus eius. Dolore ea explicabo, ratione non
+          officiis nulla.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-3xl lg:text-6xl mb-4">Se puede uno inflar</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          error accusamus iste eligendi quia esse vitae adipisci, pariatur in
+          cumque voluptas possimus eius. Dolore ea explicabo, ratione non
+          officiis nulla.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-3xl lg:text-6xl mb-4">Evita los chochos</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          error accusamus iste eligendi quia esse vitae adipisci, pariatur in
+          cumque voluptas possimus eius. Dolore ea explicabo, ratione non
+          officiis nulla.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-3xl lg:text-6xl mb-4">
+          La mente se ejercita también
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          error accusamus iste eligendi quia esse vitae adipisci, pariatur in
+          cumque voluptas possimus eius. Dolore ea explicabo, ratione non
+          officiis nulla.
+        </p>
+      </div>
+    </CircularGallery>
   );
 }
-
-const Text = ({
-  children,
-  style,
-}: {
-  style?: Record<string, MotionValue<any>>;
-  children: ReactNode;
-}) => {
-  return (
-    <motion.p style={style} className="text-xs lg:text-lg absolute top-0">
-      {children}
-    </motion.p>
-  );
-};
-
-const Emoji = ({
-  children,
-  style,
-}: {
-  style?: Record<string, MotionValue<any>>;
-  children?: ReactNode;
-}) => {
-  return (
-    <section className="relative p-8">
-      <motion.span
-        style={style}
-        className="rounded-full absolute inset-0 lg:text-6xl bg-orange-500"
-      ></motion.span>
-      <div className="absolute inset-0 flex justify-center items-center">
-        {children}
-      </div>
-    </section>
-  );
-};
-
-const Video = ({
-  src,
-  className,
-  as,
-  ...props
-}: {
-  as?: "placeholder";
-  className?: string;
-  src?: string;
-}) => {
-  return (
-    <video
-      style={{
-        aspectRatio: 4.76 / 3.54,
-      }}
-      className={cn(
-        "rounded-[50px] object-cover w-[16%] left-[-16%] relative",
-        className
-      )}
-      muted
-      autoPlay
-      loop
-      playsInline
-      src={src}
-      {...props}
-    />
-  );
-};
